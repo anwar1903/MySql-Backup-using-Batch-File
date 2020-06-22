b9f93e35584c9dc16b579ffc8ed0baa6decddbd2
anwar1903 <41816472+anwar1903@users.noreply.github.com>
2020-06-22 10:11:24 +0530

    Update BackUp.bat

--- BackUp.bat
+++ BackUp.bat
@@ -1,6 +1,6 @@
 @echo off 
 set TIMESTAMP=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
-"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" --user=root --password=bi@123 --result-file="d:\backup\db_%date:/=%_%time:~0,2%-%time:~3,2%-%time:~6,2%.sql" advocate 
-forfiles /P d:\backup\ /S /M *.sql /D -1 /C "cmd /c del @PATH"
+"C:\Program Files\MySQL\MySQL Server 8.0\bin\mysqldump" --user=root --password=yourpassword --result-file="d:\backup\db_%date:/=%_%time:~0,2%-%time:~3,2%-%time:~6,2%.sql" databaseName 
+forfiles /P d:\backup\ /S /M *.sql /D -<number of days> /C "cmd /c del @PATH"
 echo Done!
 exit
